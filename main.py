8daac036104d5f5189b974d5478fff969e30e4e7
Vladimir Kidyaev <krios2146@gmail.com>
2024-08-04 00:36:09 +0700

    feat: add `/hello` command handling & `@botusernname query` inline query handling

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
 import logging
 import os
+from uuid import uuid4
 
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    InlineQueryHandler,
+)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
-load_dotenv()
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="World")
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
-    )
+async def hello_inline_query(
+        update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    results = [
+        InlineQueryResultArticle(
+            id=str(uuid4()),
+            title="hello world",
+            input_message_content=InputTextMessageContent("Hello, World"),
+        )
+    ]
+
+    await update.inline_query.answer(results)
 
 
 if __name__ == "__main__":
+    load_dotenv()
+
     bot_token: str | None = os.getenv("TELEGRAM_BOT_TOKEN")
 
     if None:
-        raise EnvironmentError("TELEGRAM_BOT_TOKEN is not present")
+        raise EnvironmentError("'TELEGRAM_BOT_TOKEN' is not present")
 
     application = ApplicationBuilder().token(bot_token).concurrent_updates(True).build()
 
-    start_handler = CommandHandler("start", start)
-    application.add_handler(start_handler)
+    hello_handler = CommandHandler("hello", hello)
+    inline_hello_handler = InlineQueryHandler(hello_inline_query)
 
-    application.run_polling()
+    application.add_handler(hello_handler)
+    application.add_handler(inline_hello_handler)
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
