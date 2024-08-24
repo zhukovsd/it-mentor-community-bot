2c3b285077a21d714ec169ead5e11e084274145c
Vladimir Kidyaev <krios2146@gmail.com>
2024-08-25 01:40:43 +0700

    feat: add migration for filling add_project_replies table

---
+++ migrations/002_fill_add_project_replies.sql
@@ -0,0 +1,75 @@
+INSERT INTO "Add_project_replies" (
+    language, 
+    project_name, 
+    message
+) 
+VALUES ( 
+    'java', 
+    'hangman', 
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца \n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек \n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/hangman/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/hangman/) \n \- [Эталонная реализация и лекции](https://boosty.to/zhukovsd/posts/07961b26-59a9-449f-80c5-53c4c070e2b8)'
+),
+(
+    '*',
+    'hangman',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца \n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек \n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/hangman/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/hangman/)'
+),
+(
+    'java',
+    'simulation',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца \n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек \n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/simulation/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/simulation/)\n \- Эталонная реализация и лекции \- в процессе'
+),
+(
+    '*',
+    'simulation',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/simulation/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/simulation/)'
+),
+(
+    'java',
+    'currency-exchange',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/currency-exchange/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/currency-exchange/)\n \- [Эталонная реализация и лекции](https://boosty.to/zhukovsd/posts/08a542e8-5503-4331-a82b-7b6bcf04314b)'
+),
+(
+    '*',
+    'currency-exchange',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/currency-exchange/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/currency-exchange/)'
+),
+(
+    'java',
+    'tennis-scoreboard',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/tennis-scoreboard/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/tennis-scoreboard/)\n \- Эталонная реализация и лекции \- в процессе'
+),
+(
+    '*',
+    'tennis-scoreboard',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/tennis-scoreboard/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/tennis-scoreboard/)'
+),
+(
+    'java',
+    'weather-viewer',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/weather-viewer/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/weather-viewer/)\n \- Эталонная реализация и лекции \- в процессе'
+),
+(
+    '*',
+    'weather-viewer',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/weather-viewer/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/weather-viewer/)'
+),
+(
+    'java',
+    'cloud-file-storage',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/cloud-file-storage/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/cloud-file-storage/)\n \- Эталонная реализация и лекции \- в процессе'
+),
+(
+    '*',
+    'cloud-file-storage',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/cloud-file-storage/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/cloud-file-storage/)'
+),
+(
+    'java',
+    'task-tracker',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/task-tracker/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/task-tracker/)\n \- Эталонная реализация и лекции \- в процессе'
+),
+(
+    '*',
+    'task-tracker',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/java-backend-learning-course/projects/task-tracker/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/java-backend-learning-course/finished-projects/task-tracker/)'
+);
