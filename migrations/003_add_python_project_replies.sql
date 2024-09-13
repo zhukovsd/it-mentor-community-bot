c300de7b4ef62bb0c2854f8b682b61d38b2aa14e
Vladimir Kidyaev <krios2146@gmail.com>
2024-09-14 02:13:47 +0700

    feat: add replies for python projects via migrations

---
+++ migrations/003_add_python_project_replies.sql
@@ -0,0 +1,40 @@
+INSERT INTO "Add_project_replies" (
+    language, 
+    project_name, 
+    message
+) 
+VALUES (
+    'python',
+    'hangman',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца \n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек \n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/python-backend-learning-course/projects/hangman/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/python-backend-learning-course/finished-projects/hangman/)'
+),
+(
+    'python',
+    'simulation',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/python-backend-learning-course/projects/simulation/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/python-backend-learning-course/finished-projects/simulation/)'
+),
+(
+    'python',
+    'currency-exchange',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/python-backend-learning-course/projects/currency-exchange/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/python-backend-learning-course/finished-projects/currency-exchange/)'
+),
+(
+    'python',
+    'tennis-scoreboard',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/python-backend-learning-course/projects/tennis-scoreboard/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/python-backend-learning-course/finished-projects/tennis-scoreboard/)'
+),
+(
+    'python',
+    'weather-viewer',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/python-backend-learning-course/projects/weather-viewer/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/python-backend-learning-course/finished-projects/weather-viewer/)'
+),
+(
+    'python',
+    'cloud-file-storage',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/python-backend-learning-course/projects/cloud-file-storage/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/python-backend-learning-course/finished-projects/cloud-file-storage/)'
+),
+(
+    'python',
+    'task-tracker',
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/python-backend-learning-course/projects/task-tracker/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/python-backend-learning-course/finished-projects/task-tracker/)'
+);
