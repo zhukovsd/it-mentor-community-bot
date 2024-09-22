b6e478bcdd1f2b81e7ec7de0caa38faf41e5ef84
Vladimir Kidyaev <krios2146@gmail.com>
2024-09-22 20:46:50 +0700

    refactor: remove all data migrations except one

--- migrations/004_add_employment_mentoring_link_project_replies.sql
+++
@@ -1,3 +0,0 @@
-UPDATE "Add_project_replies"
-SET message = message || '\n \- [Ментороство по трудоустройству](https://telegra.ph/Mentorstvo-po-trudoustrojstvu-06-08) \- сопровождение до оффера'
-WHERE project_name = 'cloud-file-storage' OR project_name = 'task-tracker';
