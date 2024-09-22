1b43cbf3c27caa646706e9f297a41687d49d7b90
Vladimir Kidyaev <krios2146@gmail.com>
2024-09-22 18:17:55 +0700

    feat: add paid review migrations for /addproject command

---
+++ migrations/007_add_paid_reviews_kotlin_project_replies.sql
@@ -0,0 +1,27 @@
+UPDATE "Add_project_replies"
+SET message = message || '\n \- Гарантированные ревью \- @coderilya \(15$\)'
+WHERE language = 'kotlin' AND project_name = 'hangman';
+
+UPDATE "Add_project_replies"
+SET message = message || '\n \- Гарантированные ревью \- @coderilya \(15$\)'
+WHERE language = 'kotlin' AND project_name = 'simulation';
+
+UPDATE "Add_project_replies"
+SET message = message || '\n \- Гарантированные ревью \- @coderilya \(20$\), @LisaChulkova \(25$\)'
+WHERE language = 'kotlin' AND project_name = 'currency-exchange';
+
+UPDATE "Add_project_replies"
+SET message = message || '\n \- Гарантированные ревью \- @coderilya \(20$\), @LisaChulkova \(30$\)'
+WHERE language = 'kotlin' AND project_name = 'tennis-scoreboard';
+
+UPDATE "Add_project_replies"
+SET message = message || '\n \- Гарантированные ревью \- @coderilya \(25$\), @LisaChulkova \(30$\)'
+WHERE language = 'kotlin' AND project_name = 'weather-viewer';
+
+UPDATE "Add_project_replies"
+SET message = message || '\n \- Гарантированные ревью \- @coderilya \(25$\), @LisaChulkova \(40$\)'
+WHERE language = 'kotlin' AND project_name = 'cloud-file-storage';
+
+UPDATE "Add_project_replies"
+SET message = message || '\n \- Гарантированные ревью \- @coderilya \(30$\), @LisaChulkova \(40$\)'
+WHERE language = 'kotlin' AND project_name = 'task-tracker';
---
+++ migrations/008_add_paid_reviews_python_project_replies.sql
@@ -0,0 +1,11 @@
+UPDATE "Add_project_replies"
+SET message = message || '\n \- Гарантированные ревью \- @apostol\_fet \(15$\)'
+WHERE language = 'python' AND project_name = 'hangman';
+
+UPDATE "Add_project_replies"
+SET message = message || '\n \- Гарантированные ревью \- @apostol\_fet \(15$\)'
+WHERE language = 'python' AND project_name = 'simulation';
+
+UPDATE "Add_project_replies"
+SET message = message || '\n \- Гарантированные ревью \- @apostol\_fet \(15$\)'
+WHERE language = 'python' AND project_name = 'currency-exchange';
