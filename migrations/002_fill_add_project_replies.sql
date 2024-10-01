6a6b8124f6083b6946e1b1d0cb4046b596239616
Sergey Zhukov <zhukovsd@gmail.com>
2024-10-01 21:27:53 +0400

    migrations: valid python roadmap link for currency exchange

--- migrations/002_fill_add_project_replies.sql
+++ migrations/002_fill_add_project_replies.sql
@@ -82,6 +82,11 @@ INSERT INTO "Add_project_replies" (language, project_name, message) VALUES (
 );
 INSERT INTO "Add_project_replies" (language, project_name, message) VALUES (
     'python', 
+    'currency-exchange', 
+    'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/python-backend-learning-course/projects/currency-exchange/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/python-backend-learning-course/finished-projects/simulation/)'
+);
+INSERT INTO "Add_project_replies" (language, project_name, message) VALUES (
+    'python', 
     'tennis-scoreboard', 
     'Спасибо за проект, ссылка на него будет опубликована в итогах следующего месяца\n\nПроверьте, что проект запускается и к репозиторую есть публичный доступ, это повышает шансы получить фидбек\n\nРесурсы для работы над ошибками:\n \- [Чеклист типовых проблем в конце ТЗ](https://zhukovsd.github.io/python-backend-learning-course/projects/tennis-scoreboard/#%d1%87%d0%b5%d0%ba%d0%bb%d0%b8%d1%81%d1%82-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%bc%d0%be%d0%bf%d1%80%d0%be%d0%b2%d0%b5%d1%80%d0%ba%d0%b8)\n \- [Список ревью](https://zhukovsd.github.io/python-backend-learning-course/finished-projects/tennis-scoreboard/)'
 );
