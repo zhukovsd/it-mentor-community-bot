986760f24a48f4659ae8d09d39cd0c7743f9b763
Asenim <pasha_me1998@mail.ru>
2024-10-21 23:23:47 +0400

    Переписан метод для получения данныых из url

---
+++ src/get_data_completed_project/project_data_serializer.py
@@ -0,0 +1,16 @@
+class ProjectDataSerializer:
+    def __init__(self, name_owner_repo, url_owner_repo,
+                 repository_name, repository_url, program_lang_project):
+        """
+        Хранит корректные данные из url для дальнейшей работы с таблицами
+        :param repository_name: Проект/Имя репозитория
+        :param name_owner_repo: Имя владельца репозитория
+        :param repository_url: Ссылка на репозиторий
+        :param url_owner_repo: Ссылка на владельца репозитория
+        :param program_lang_project: Язык программирования проекта
+        """
+        self.repository_name = repository_name
+        self.name_owner_repo = name_owner_repo
+        self.repository_url = repository_url
+        self.url_owner_repo = url_owner_repo
+        self.program_lang_project = program_lang_project
