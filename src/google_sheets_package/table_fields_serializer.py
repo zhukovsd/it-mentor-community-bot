99dcd4dd6c059f637fc0d2e6a639ace3627a7d7c
Alfob <98178864+Asenim@users.noreply.github.com>
2024-11-04 02:32:39 +0400

    Update src/google_sheets_package/table_fields_serializer.py
    Co-authored-by: Vladimir Kidyaev <krios2146@gmail.com>

--- src/google_sheets_package/table_fields_serializer.py
+++ src/google_sheets_package/table_fields_serializer.py
@@ -1,4 +1,4 @@
-class TableFieldsSerializer:
+class GSheetFields:
     def __init__(self, object_opened_sheet, number_row_cell_excel: int | str):
         """
         Для удобного взаимодействия с таблицей, содержит в себе Поля
