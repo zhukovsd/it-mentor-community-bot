823be0a91c9d38884bf713a243ea0c206ec8f0a6
Vladimir Kidyaev <krios2146@gmail.com>
2024-11-09 23:59:41 +0700

    feat: add INTERVIEW_COLLECTION_SPREADSHEET_ID environment variable

--- src/env.py
+++ src/env.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Константы для работы с gsheet
-JSON_KEY_GOOGLE_API: str | None = os.environ.get('JSON_KEY_GOOGLE_API')
-ADD_TO_SHEET_NAME: str | None = os.environ.get('ADD_TO_SHEET_NAME')
+JSON_KEY_GOOGLE_API: str | None = os.environ.get("JSON_KEY_GOOGLE_API")
+ADD_TO_SHEET_NAME: str | None = os.environ.get("ADD_TO_SHEET_NAME")
 
 # Константы для подключения к БД
 POSTGRES_USER: str | None = os.environ.get("POSTGRES_USER")
@@ -14,3 +14,6 @@ POSTGRES_DB: str | None = os.environ.get("POSTGRES_DB")
 POSTGRES_HOST: str | None = os.environ.get("POSTGRES_HOST")
 POSTGRES_PORT: str | None = os.getenv("POSTGRES_PORT")
 
+INTERVIEW_COLLECTION_SPREADSHEET_ID: str | None = os.getenv(
+    "INTERVIEW_COLLECTION_SPREADSHEET_ID"
+)
