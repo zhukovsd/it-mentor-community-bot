1bfeb800bc6e2203acf564e7937977ecb7b83c37
Vladimir Kidyaev <krios2146@gmail.com>
2024-11-10 00:03:53 +0700

    feat: add new handler to the main.py

--- src/main.py
+++ src/main.py
@@ -13,11 +13,18 @@ from telegram.ext import (
     ContextTypes,
     CommandHandler,
     InlineQueryHandler,
+    MessageHandler,
+    filters,
 )
+
+from log_config import log_config
 
 from add_project_handler import add_project
 from add_project_handler import ADD_PROJECT_COMMAND_NAME
-from log_config import log_config
+from search_interviews_with_question_handler import search_interviews_with_question
+from search_interviews_with_question_handler import (
+    SEARCH_INTERVIEWS_WITH_QUESTION_COMMAND_REGEXP,
+)
 
 log_config()
 log = logging.getLogger(__name__)
@@ -48,9 +55,14 @@ if __name__ == "__main__":
     application = ApplicationBuilder().token(bot_token).concurrent_updates(True).build()
 
     add_project_handler = CommandHandler(ADD_PROJECT_COMMAND_NAME, add_project)
+    search_interviews_with_question_handler = MessageHandler(
+        filters.COMMAND & filters.Regex(SEARCH_INTERVIEWS_WITH_QUESTION_COMMAND_REGEXP),
+        search_interviews_with_question,
+    )
     inline_hello_handler = InlineQueryHandler(hello_inline_query)
 
     application.add_handler(inline_hello_handler)
     application.add_handler(add_project_handler)
+    application.add_handler(search_interviews_with_question_handler)
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
