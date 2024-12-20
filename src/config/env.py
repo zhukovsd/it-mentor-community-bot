a7637c76ea3f9aa51a7f131ac7747d42df7e5015
Vladimir Kidyaev <krios2146@gmail.com>
2024-12-20 20:50:11 +0700

    feat: add github environment variables to the env.py

--- src/config/env.py
+++ src/config/env.py
@@ -23,9 +23,17 @@ POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
 INTERVIEW_COLLECTION_SPREADSHEET_ID: str = os.getenv("INTERVIEW_COLLECTION_SPREADSHEET_ID")  # fmt: skip
 SEARCH_INTERVIEW_QUESTIONS_COMMAND_CHAT_IDS: str = os.getenv("SEARCH_INTERVIEW_QUESTIONS_COMMAND_CHAT_IDS")  # fmt: skip
 
+INTERVIEW_PREP_SITE_REPO_OWNER: str = os.getenv("INTERVIEW_PREP_SITE_REPO_OWNER")
+INTERVIEW_PREP_SITE_REPO_NAME: str = os.getenv("INTERVIEW_PREP_SITE_REPO_NAME")
+
+GITHUB_COMMUNITY_BOT_ACCESS_TOKEN: str = os.getenv("GITHUB_COMMUNITY_BOT_ACCESS_TOKEN")
+
+QUESTIONS_POPULARITY_UPDATE_ALLOWED_USER_IDS: str = os.getenv("QUESTIONS_POPULARITY_UPDATE_ALLOWED_USER_IDS")  # fmt: skip
+
 # fmt: off
 assert TELEGRAM_BOT_TOKEN is not None, "TELEGRAM_BOT_TOKEN environment variable is not set"
 assert PROJECTS_REVIEWS_COLLECTION_CHAT_ID is not None, "PROJECTS_REVIEWS_COLLECTION_CHAT_ID environment variable is not set"
+assert ADD_PROJECT_ALLOWED_USER_IDS is not None, "ADD_PROJECT_ALLOWED_USER_IDS environment variable is not set"
 
 assert JSON_KEY_GOOGLE_API is not None, "JSON_KEY_GOOGLE_API environment variable is not set"
 # assert ADDED_PROJECTS_SPREADSHEET_ID is not None, "ADDED_PROJECTS_SPREADSHEET_ID environment variable is not set"
@@ -38,3 +46,10 @@ assert POSTGRES_PORT is not None, "POSTGRES_PORT environment variable is not set
 
 assert INTERVIEW_COLLECTION_SPREADSHEET_ID is not None, "INTERVIEW_COLLECTION_SPREADSHEET_ID environment variable is not set"
 assert SEARCH_INTERVIEW_QUESTIONS_COMMAND_CHAT_IDS is not None, "SEARCH_INTERVIEW_QUESTIONS_COMMAND_CHAT_IDS environment variable is not set"
+
+assert INTERVIEW_PREP_SITE_REPO_OWNER is not None, "INTERVIEW_PREP_SITE_REPO_OWNER environment variable is not set"
+assert INTERVIEW_PREP_SITE_REPO_NAME is not None, "INTERVIEW_PREP_SITE_REPO_NAME environment variable is not set"
+
+assert GITHUB_COMMUNITY_BOT_ACCESS_TOKEN is not None, "GITHUB_COMMUNITY_BOT_ACCESS_TOKEN environment variable is not set"
+
+assert QUESTIONS_POPULARITY_UPDATE_ALLOWED_USER_IDS is not None, "QUESTIONS_POPULARITY_UPDATE_ALLOWED_USER_IDS environment variable is not set"
