9dcac8c5103c7fb98444e0c7bc1d1c789929afb6
Asenim <pasha_me1998@mail.ru>
2024-12-30 09:32:07 +0400

    Немного доработаны кейсы + изменена чутка структура тестов

--- tests/fixtures/google_sheet_fixtures/parse_url_fixtures.py
+++ tests/fixtures/google_sheet_fixtures/parse_url_fixtures.py
@@ -1,3 +1,8 @@
+"""
+Тест кейсы для тестов модуля parse_url_from_message.py из пакета src/google_sheet
+
+Сами тесты находятся tests/google_sheet_tests/test_parse_url.py
+"""
 import pytest
 
 
@@ -13,9 +18,13 @@ def approved_parse_url():
     test_cases = []
 
     for url in url_samples:
-        text_sample = f"Сделал проект Simulation, ссылка на проект - {url}, Кому не сложно будет - посмотрите реализацию"
-        test_cases.append(text_sample)
-        test_cases.append(url)
+        text_samples = [
+            f"Сделал проект Simulation, ссылка на проект - {url}, Кому не сложно будет - посмотрите реализацию",
+            f"Строка с двумя url, первая - https://random_url.com, вторая - {url}? и еще немного текста",
+            ]
+        for text_sample in text_samples:
+            test_cases.append(text_sample)
+            test_cases.append(url)
 
     return test_cases
 
--- tests/google_sheet_tests/test_parse_url.py
+++ tests/google_sheet_tests/test_parse_url.py
@@ -1,7 +1,12 @@
+"""
+Тестируем модуль parse_url_from_message.py из пакета src/google_sheet
+
+Тест кейсы для текущего модуля расположены в фикстурах по пути:
+    tests/fixtures/google_sheet_fixtures в модуле parse_url_fixtures.py
+"""
 from src.google_sheet.parse_url_from_message import parse_url_from_message
-from tests.fixtures.parse_url_fixtures import (
-    approved_parse_url,
-    skip_parse_url
+from tests.fixtures.google_sheet_fixtures.parse_url_fixtures import (
+    approved_parse_url, skip_parse_url
 )
 
 
